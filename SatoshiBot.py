01038774b585d97d7b307786357566eb1c76e5b4
Michael Janos <spikeybadooks@gmail.com>
2017-11-03 21:16:44 -0400

    Removed individual methods and inherited from discord.Client. Now keeps track of last price and last fetch. Will only fetch new prices if time since last check was more than 1 minute ago. Also changes chat when @ mentioning based on previous price vs current price.

--- SatoshiBot.py
+++ SatoshiBot.py
@@ -5,125 +5,159 @@ import urllib.request
 import json
 import datetime
 import time
+import random
 
-client = discord.Client()
+class SatoshiBot(discord.Client):
+    def __init__(self,*args,**kwargs):
+        self.crypto_links = {'BTC':"https://api.coinbase.com/v2/exchange-rates?currency=BTC",
+                            'ETH':"https://api.coinbase.com/v2/exchange-rates?currency=ETH",
+                            'LTC':"https://api.coinbase.com/v2/exchange-rates?currency=LTC"}
 
-async def get_crypto_data():
-    crypto_links = {'BTC':"https://api.coinbase.com/v2/exchange-rates?currency=BTC",
-                    'ETH':"https://api.coinbase.com/v2/exchange-rates?currency=ETH",
-                    'LTC':"https://api.coinbase.com/v2/exchange-rates?currency=LTC"}
-    exchange_rates = {'BTC':{},'ETH':{},'LTC':{}}
+        self.positive_messages = ["McAfee might be on to something...",
+                                    "EAT THE RICH",
+                                    "https://www.youtube.com/watch?v=y2ak_oBeC-I",
+                                    "https://www.youtube.com/watch?v=HgzGwKwLmgM",
+                                    "Have you heard about our lord and saviour, Bitcoin?"
+                                    ]
 
-    for currency,link in crypto_links.items():
-        data = urllib.request.urlopen(link)
-        parsed_data = json.load(data)
-        for k,v in parsed_data.get('data').get('rates').items():
-            exchange_rates[currency][k] = v.replace(',','')
+        self.neutral_messages = ["https://ei.marketwatch.com//Multimedia/2017/08/14/Photos/NS/MW-FS292_bitcoi_20170814143201_NS.gif?uuid=dd35299a-811e-11e7-ac6f-9c8e992d421e","YES. UMM HI???? HELLO?????"]
 
-    return exchange_rates
+        self.negative_messages = ["Yeah, yeah I know...","I SWEAR I DIDN'T DO THIS PLEASE I'M A GOOD BOT","Blame Trump","Maybe Dimon was right...","There's a reason nobody knows my identity","https://media.coindesk.com/uploads/2014/03/grumpy-nakamoto.png"]
 
-# @client.event
-# async def hourly_message():
-#     msg = None
-#     while True:
-#         price_string = time.strftime('```%b %d, %Y -- %I:%M%p```')
-#
-#         price_string = "%s\n```1 BTC = $%0.2f USD\n1 BTC = %0.5f ETH\n1 BTC = %0.5f LTC\n" % (price_string,
-#                                                                                             float(data.get('data').get('rates').get('USD').replace(',','')),
-#                                                                                             float(data.get('data').get('rates').get('ETH').replace(',','')),
-#                                                                                             float(data.get('data').get('rates').get('LTC').replace(',','')),
-#                                                                                             )
-#
-#         #GET ETH
-#         btcdata = urllib.request.urlopen("https://api.coinbase.com/v2/prices/ETH-USD/spot")
-#         data = json.load(btcdata)
-#         price_string = "%s\n1 ETH = $%0.2f USD" % (price_string,float(data.get('data').get('amount').replace(',','')))
-#
-#         #GET LTC
-#         btcdata = urllib.request.urlopen("https://api.coinbase.com/v2/prices/LTC-USD/spot")
-#         data = json.load(btcdata)
-#         price_string = "%s\n1 LTC = $%0.2f USD" % (price_string,float(data.get('data').get('amount').replace(',','')))
-#
-#         for channel in client.get_all_channels():
-#             if channel.name == "bitcoinchat":
-#                 for pin in await client.pins_from(channel):
-#                     if pin.author == client.user:
-#                         msg = pin
-#                 if msg:
-#                     await client.edit_message(msg,price_string + "```")
-#                 else:
-#                     msg = await client.send_message(channel,price_string + "```")
-#                 await client.pin_message(msg)
-#         current_minute = datetime.datetime.today().minute
-#         current_second = datetime.datetime.today().second
-#         seconds_left = (59-current_minute)*60 + (60-current_second)
-#         await asyncio.sleep(seconds_left)
+        self.exchange_rates = {'BTC':{},'ETH':{},'LTC':{}}
 
-# @client.event
-# async def milestone_message():
-#     msg = None
-#     # for channel in client.get_all_channels():
-#     #     if channel.name == "bitcoinchat":
-#     #         for message in await client.logs_from(channel,limit=5000):
-#     #             if message.author == client.user:
-#     #                 #re.match("")
-#     #                 pass
-#     # while True:
-#     #     await asyncio.sleep(600)
-#     print("inloop")
-#     # for channel in client.get_all_channels():
-#     #     if channel.name == "botchat-adminonly":
-#     #         await client.send_message(channel,"@Spikeybadooks test")
+        self.previous_btc = None
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+        self.latest_fetch = None
 
-@client.event
-async def on_message(message):
-    if client.user in message.mentions:
-        msg1 = await client.send_message(message.channel,'YES. UMM HI???? HELLO?????')
-        msg2 = await client.send_message(message.channel,"...")
+        super().__init__(*args,**kwargs)
 
-    if message.content.lower().startswith('$btc'):
-        msg = await client.send_message(message.channel, 'Getting BTC Price...')
-    elif message.content.lower().startswith('$eth'):
-        msg = await client.send_message(message.channel, 'Getting ETH Price...')
-    elif message.content.lower().startswith('$ltc'):
-        msg = await client.send_message(message.channel, 'Getting LTC Price...')
+    async def get_crypto_data(self):
+        if self.latest_fetch is None or datetime.datetime.now() - self.latest_fetch > datetime.timedelta(minutes=1):
+            self.latest_fetch = datetime.datetime.now()
+            for currency,link in self.crypto_links.items():
+                data = urllib.request.urlopen(link)
+                parsed_data = json.load(data)
+                for k,v in parsed_data.get('data').get('rates').items():
+                    self.exchange_rates[currency][k] = v.replace(',','')
+        else:
+            print("Skipping fetch")
 
+    # @client.event
+    # async def hourly_message():
+    #     msg = None
+    #     while True:
+    #         price_string = time.strftime('```%b %d, %Y -- %I:%M%p```')
+    #
+    #         price_string = "%s\n```1 BTC = $%0.2f USD\n1 BTC = %0.5f ETH\n1 BTC = %0.5f LTC\n" % (price_string,
+    #                                                                                             float(data.get('data').get('rates').get('USD').replace(',','')),
+    #                                                                                             float(data.get('data').get('rates').get('ETH').replace(',','')),
+    #                                                                                             float(data.get('data').get('rates').get('LTC').replace(',','')),
+    #                                                                                             )
+    #
+    #         #GET ETH
+    #         btcdata = urllib.request.urlopen("https://api.coinbase.com/v2/prices/ETH-USD/spot")
+    #         data = json.load(btcdata)
+    #         price_string = "%s\n1 ETH = $%0.2f USD" % (price_string,float(data.get('data').get('amount').replace(',','')))
+    #
+    #         #GET LTC
+    #         btcdata = urllib.request.urlopen("https://api.coinbase.com/v2/prices/LTC-USD/spot")
+    #         data = json.load(btcdata)
+    #         price_string = "%s\n1 LTC = $%0.2f USD" % (price_string,float(data.get('data').get('amount').replace(',','')))
+    #
+    #         for channel in client.get_all_channels():
+    #             if channel.name == "bitcoinchat":
+    #                 for pin in await client.pins_from(channel):
+    #                     if pin.author == client.user:
+    #                         msg = pin
+    #                 if msg:
+    #                     await client.edit_message(msg,price_string + "```")
+    #                 else:
+    #                     msg = await client.send_message(channel,price_string + "```")
+    #                 await client.pin_message(msg)
+    #         current_minute = datetime.datetime.today().minute
+    #         current_second = datetime.datetime.today().second
+    #         seconds_left = (59-current_minute)*60 + (60-current_second)
+    #         await asyncio.sleep(seconds_left)
 
+    # @client.event
+    # async def milestone_message():
+    #     msg = None
+    #     # for channel in client.get_all_channels():
+    #     #     if channel.name == "bitcoinchat":
+    #     #         for message in await client.logs_from(channel,limit=5000):
+    #     #             if message.author == client.user:
+    #     #                 #re.match("")
+    #     #                 pass
+    #     # while True:
+    #     #     await asyncio.sleep(600)
+    #     print("inloop")
+    #     # for channel in client.get_all_channels():
+    #     #     if channel.name == "botchat-adminonly":
+    #     #         await client.send_message(channel,"@Spikeybadooks test")
 
-    exchange_rates = await get_crypto_data()
-    if message.content.lower().startswith('$btc'):
-        price_string = "1 BTC = $%0.2f USD" % float(exchange_rates['BTC']['USD'])
-        await client.edit_message(msg,price_string)
-    elif message.content.lower().startswith('$eth'):
-        price_string = "1 ETH = %0.5f BTC" % float(exchange_rates['ETH']['BTC'])
-        await client.edit_message(msg,price_string)
-    elif message.content.lower().startswith('$ltc'):
-        price_string = "1 LTC = %0.5f BTC" % float(exchange_rates['LTC']['BTC'])
-        await client.edit_message(msg,price_string)
-    elif message.content.lower().startswith('$help'):
-        await client.send_message(message.channel,'BTC -> USD = $btc\nETH -> BTC = $eth\nLTC -> BTC = $ltc')
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-    if client.user in message.mentions:
-        price_string = time.strftime('```%b %d, %Y -- %I:%M%p```')
-        #GET BTC
+    async def on_message(self,message):
+        if self.user in message.mentions or message.content.lower().startswith('$btc') or message.content.lower().startswith('$eth') or message.content.lower().startswith('$ltc') or message.content.lower().startswith('$help'):
+            if self.user in message.mentions:
+                if self.previous_btc is None:
+                    msg1 = await self.send_message(message.channel,'YES. UMM HI???? HELLO?????')
+                else:
+                    msg1 = await self.send_message(message.channel,"...")
+                msg2 = await self.send_message(message.channel,"...")
 
-        price_string = "%s\n```1 BTC = $%0.2f USD\n1 BTC = %0.5f ETH\n1 BTC = %0.5f LTC\n" % (price_string,
-                                                                                            float(exchange_rates['BTC']['USD']),
-                                                                                            float(exchange_rates['BTC']['ETH']),
-                                                                                            float(exchange_rates['BTC']['LTC']),
-                                                                                            )
-        #GET ETH
-        price_string = "%s\n1 ETH = $%0.2f USD" % (price_string,float(exchange_rates['ETH']['USD']))
+            if message.content.lower().startswith('$btc'):
+                msg = await self.send_message(message.channel, 'Getting BTC Price...')
+            elif message.content.lower().startswith('$eth'):
+                msg = await self.send_message(message.channel, 'Getting ETH Price...')
+            elif message.content.lower().startswith('$ltc'):
+                msg = await self.send_message(message.channel, 'Getting LTC Price...')
 
-        #GET LTC
-        price_string = "%s\n1 LTC = $%0.2f USD" % (price_string,float(exchange_rates['LTC']['USD']))
-        await client.edit_message(msg2,price_string + "```")
+            await self.get_crypto_data()
+            current_btc = float(self.exchange_rates['BTC']['USD'])
 
-client.run(os.environ['SATOSHI_KEY'])
+            if self.user in message.mentions:
+                if not self.previous_btc is None:
+                    if self.previous_btc <= current_btc:
+                        await self.edit_message(msg1,random.choice(self.positive_messages))
+                    elif self.previous_btc > current_btc:
+                        await self.edit_message(msg1,random.choice(self.negative_messages))
+
+            self.previous_btc = current_btc
+
+            if message.content.lower().startswith('$btc'):
+                price_string = "1 BTC = $%0.2f USD" % float(self.exchange_rates['BTC']['USD'])
+                await self.edit_message(msg,price_string)
+            elif message.content.lower().startswith('$eth'):
+                price_string = "1 ETH = %0.5f BTC" % float(self.exchange_rates['ETH']['BTC'])
+                await self.edit_message(msg,price_string)
+            elif message.content.lower().startswith('$ltc'):
+                price_string = "1 LTC = %0.5f BTC" % float(self.exchange_rates['LTC']['BTC'])
+                await self.edit_message(msg,price_string)
+            elif message.content.lower().startswith('$help'):
+                await self.send_message(message.channel,'```BTC -> USD = $btc\nETH -> BTC = $eth\nLTC -> BTC = $ltc\n@Satoshi for more info```')
+
+            if self.user in message.mentions:
+                price_string = time.strftime('```%b %d, %Y -- %I:%M%p```')
+                #GET BTC
+
+                price_string = "%s\n```1 BTC = $%0.2f USD\n1 BTC = %0.5f ETH\n1 BTC = %0.5f LTC\n" % (price_string,
+                                                                                                    float(self.exchange_rates['BTC']['USD']),
+                                                                                                    float(self.exchange_rates['BTC']['ETH']),
+                                                                                                    float(self.exchange_rates['BTC']['LTC']),
+                                                                                                    )
+                #GET ETH
+                price_string = "%s\n1 ETH = $%0.2f USD" % (price_string,float(self.exchange_rates['ETH']['USD']))
+
+                #GET LTC
+                price_string = "%s\n1 LTC = $%0.2f USD" % (price_string,float(self.exchange_rates['LTC']['USD']))
+                await self.edit_message(msg2,price_string + "```")
+
+if __name__=="__main__":
+    client = SatoshiBot()
+    client.run(os.environ['SATOSHI_KEY'])
